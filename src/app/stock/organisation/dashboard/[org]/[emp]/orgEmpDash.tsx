79516958cdd7e1c0c9c5510a243f580df3cdd34d
Leo <leodarkseid@gmail.com>
2023-07-28 00:30:48 +0100

    complted org dashboard

--- src/app/stock/organisation/dashboard/[org]/[emp]/orgEmpDash.tsx
+++ src/app/stock/organisation/dashboard/[org]/[emp]/orgEmpDash.tsx
@@ -11,7 +11,25 @@ export function EmpDashBoard(props: EmpDashProps) {
         <>
             <div className="bg-primary rounded text-center text-white p-2 w-100">This is the Address of Employee</div>
             <div className={styles.main}>
-                <div className={styles.main_grid1}>first </div>
+                <div className={styles.main_grid1}>
+                    <div className={styles.main_grid1__col}>
+                        <div className={styles.main_grid__timer_box}>
+                            <div className={styles.main_grid__timer_box__small_grid}>
+                                <div className={styles.main_grid__timer_box_days__val}>21</div>
+                                <div className={styles.main_grid__timer_box_hours__val}>21</div>
+                                <div className={styles.main_grid__timer_box_mins__val}>21</div>
+                                <div className={styles.main_grid__timer_box_days}>Days</div>
+                                <div className={styles.main_grid__timer_box_hours}>Hours</div>
+                                <div className={styles.main_grid__timer_box_mins}>Min</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
                 <div className={styles.main_grid2}>
                     <div className={styles.main_grid2__col}>
                         <div className={styles.main_grid2__row}>
@@ -25,7 +43,7 @@ export function EmpDashBoard(props: EmpDashProps) {
 
                         </div>
                         <div className={styles.main_grid2__row}>
-                            <div className={styles.main_grid2__row_display}><Form className="w-100"><Form.Control placeholder="0x0000..."/></Form></div>
+                            <div className={styles.main_grid2__row_display}><Form className="w-100"><Form.Control placeholder="0x0000..." /></Form></div>
                             <div className={styles.main_grid2__row_button}>Transfer</div>
 
                         </div>
