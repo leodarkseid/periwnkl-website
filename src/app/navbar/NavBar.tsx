e9df8fda0858e848b6d32bd81cabfaf908bc7d2b
Leo <leodarkseid@gmail.com>
2023-08-08 22:58:47 +0100

    front page

--- src/app/navbar/NavBar.tsx
+++ src/app/navbar/NavBar.tsx
@@ -64,12 +64,12 @@ export default function NavBar() {
                     <Navbar.Brand href="#home">
                         <Image
                             alt=""
-                            src="/img/logo.svg"
+                            src="../image/logoBlue.png"
                             width="30"
                             height="30"
                             className="d-inline-block align-top"
                         />{' '}
-                        PeriWnkl
+                        Periwnkl
                     </Navbar.Brand>
                 </Container>
             </Navbar>
