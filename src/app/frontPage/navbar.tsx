a14bdffbb426d27e77040a7cd48d8926c8567f11
Leo <leodarkseid@gmail.com>
2023-08-08 22:36:15 +0100

    front page

--- src/app/frontPage/navbar.tsx
+++ src/app/frontPage/navbar.tsx
@@ -1,5 +1,20 @@
 export default function FPNavbar() {
     return (
-        <div>This is the brand new wagepay app</div>
+        <>
+            <Navbar className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src="/img/logo.svg"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />{' '}
+                        React Bootstrap
+                    </Navbar.Brand>
+                </Container>
+            </Navbar>
+        </>
     )
 }
