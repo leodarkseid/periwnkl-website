1681eefda39c5a9276a4d3c9512d88292380a881
Leo <leodarkseid@gmail.com>
2023-08-08 22:44:02 +0100

    front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import FPBody from "./frontPage/body";
 import FPFooter from "./frontPage/footer";
 import FPHeader from "./frontPage/header";
-import FPNavbar from "./frontPage/navbar";
 
 export default function Page() {
     return (
