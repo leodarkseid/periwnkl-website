e73cebef39c785965fe99ad849147e560e31f19d
Leo <leodarkseid@gmail.com>
2023-08-13 23:35:37 +0100

    update

--- src/app/frontPage/header.tsx
+++ src/app/frontPage/header.tsx
@@ -1,10 +1,10 @@
 export default function FPHeader() {
     return (
         <>
-        <h1>Periwnkl
-        
-        is a project inspired by novel EVM Applications like Delegation, Abstraction to provide free composable services that brings real life use cases Onchain</h1>
-        <h2>Crypto doesn't have to be all Scam, We are building for real builders</h2>
+            <h1>Periwnkl</h1>
+
+            is a project inspired by novel EVM Applications like Delegation, Abstraction to provide free composable services that brings real life use cases Onchain
+            <h2>Crypto doesn't have to be all Scam, We are building for real builders</h2>
         </>
     )
 }
