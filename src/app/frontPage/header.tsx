4c58eb628b72e5de0dcb66fd731fd51abe4af8b2
Leo <leodarkseid@gmail.com>
2023-08-14 01:49:24 +0100

    update

--- src/app/frontPage/header.tsx
+++ src/app/frontPage/header.tsx
@@ -1,10 +1,21 @@
+import styles from "./css/header.module.css"
+
 export default function FPHeader() {
     return (
         <>
-            <div><h1>Periwnkl</h1></div>
+            <div >
+                <h2 className={styles.animate_character}>Periwnkl</h2>
 
-            is a project inspired by novel EVM Applications like Delegation, Abstraction to provide free composable services that brings real life use cases Onchain
-            <h2>Crypto doesn't have to be all Scam, We are building for real builders</h2>
+
+
+                <span>is a project inspired by novel EVM Applications like Delegation, Abstraction to provide free composable services that brings real life use cases Onchain</span>
+                <h2>Crypto does not have to be
+                    <div className="message">
+                        <div class="word1">close</div>
+                        <div class="word2">code</div>
+                        <div class="word3">creating</div>
+                    </div>all Scam </h2> <h2> We are building for real builders</h2>
+            </div>
         </>
     )
 }
